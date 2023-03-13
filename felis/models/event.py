5e459b6ca05c700eed6e1bb40841768fb7033848
Iori Ichinose <milk.ichinose.iori@gmail.com>
2023-03-13 13:47:25 +0800

    feat: add session property to events

--- felis/models/event.py
+++ felis/models/event.py
@@ -10,3 +10,14 @@ class BaseEvent(BaseModel):
     detail_type: str
     sub_type: str
     self: Self | None
+
+    @property
+    def session_id(self) -> str:
+        group_id = getattr(self, "group_id", None)
+        user_id = getattr(self, "user_id", None)
+        session_id = ["felis"]
+        if group_id is not None:
+            session_id.append(f"g_{group_id}")
+        if user_id is not None:
+            session_id.append(f"u_{user_id}")
+        return ".".join(session_id)
